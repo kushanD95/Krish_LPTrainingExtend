53d758cea64ca6636f6d7edef9c246d13d90cac3
SanukaKushan <kushanktc@gmail.com>
2021-01-16 16:40:48 +0530

    Number Reverser -Completed-

---
+++ NumberReverse/NumberReverser.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class NumberReverser {
+
+    private int originalValue = 0;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        NumberReverser numberReverser = new NumberReverser();
+
+        System.out.println("Enter the Original Integer Value : ");
+
+        numberReverser.originalValue = scanner.nextInt();
+
+        System.out.println(numberReverser.numberReverse()+"  <== Reversed Integer Value");
+
+
+    }
+
+
+    private int numberReverse() {
+        int revNum = 0, rem, dividand
+                ;
+
+        if(originalValue / 10 != 0) {
+            dividand = originalValue;
+
+            while(dividand != 0) {
+                rem = dividand % 10;
+                revNum = (revNum * 10) + rem;
+                dividand /= 10;
+            }
+
+            return revNum;
+        } else {
+            return originalValue;
+        }
+    }
+
+
+
+
+
+}
